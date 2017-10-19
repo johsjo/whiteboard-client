360be7a86ff56e890bec70f7b9d215929675f43e
Johan Sjölander <johsjo83@gmail.com>
2017-10-19 13:19:14 +0200

    Updated reduxStore

--- src/reduxStore/config/index.js
+++ src/reduxStore/config/index.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
-import notes from './notes';
+// import notes from './notes';
+// import whiteboards from './whiteboards';
+
+import reducerNotes from './notes';
+import reducerWhiteboards from './whiteboards';
 
 const reducer = combineReducers({
-  notes,
+  reducerNotes,
+  reducerWhiteboards,
 });
 
 export default reducer;
--- src/reduxStore/index.js
+++ src/reduxStore/index.js
@@ -1,13 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import reducer, { replaceNotes } from './config/notes';
+import reducer from './config/';
+import { replaceNotes } from './config/notes';
+import { updateWhiteboards } from './config/whiteboards';
 
 /* eslint-disable no-underscore-dangle */
-
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 /* eslint-enable */
 
 store.dispatch(replaceNotes());
+store.dispatch(updateWhiteboards());
+
+console.log('Redux index');
 
 export default store;
